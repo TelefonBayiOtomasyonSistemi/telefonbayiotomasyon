90436e38b2d0950641778d4dbe4f8b637f08968b
ismailaydin52 <ismailaydin52002@gmail.com>
2024-05-04 16:19:59 +0300

    Update user_login.php

--- Kodlar/php/contact/user_login.php
+++ Kodlar/php/contact/user_login.php
@@ -5,6 +5,7 @@
 $email = $_POST['email'];
 $sifre = $_POST['password'];
 
+
 // Veritabanı bağlantısı
 $servername = "localhost"; // Veritabanı sunucusu (genellikle localhost)
 $username = "root"; // Veritabanı kullanıcı adı
@@ -23,14 +24,48 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM kullanici WHERE email='$email' AND sifre='$sifre'";
 $result = $conn->query($sql);
 
+
+$sqlgiris ="SELECT hesap_tipi FROM kullanici WHERE email='$email' AND sifre='$sifre'";
+$resultgiris = $conn->query($sqlgiris);
+
+
+
+
+
 // Kullanıcı var mı kontrol et
 if ($result->num_rows > 0) {
     // Kullanıcıyı giriş sayfasına yönlendir
     echo "<script>alert('Giriş başarılı! Anasayfaya yönlendiriliyorsunuz...');</script>";
-    
+
+    if ($resultgiris->num_rows > 0) {
+        // Sonuç kümesinde en az bir satır varsa, verileri işle
+        while ($row = $resultgiris->fetch_assoc()) {
+            // Her satırı işle
+            $hesap_tipi =$row["hesap_tipi"] ;
+            echo $hesap_tipi;
+        }}
+
+if ($hesap_tipi == "müsteri") {
     echo "<script>setTimeout(function() { window.location.href = '../home/home.php'; }, );</script>";
-    exit();
-} else {
+}
+else if ($hesap_tipi== "bayi") {
+    echo "<script>setTimeout(function() { window.location.href = '../users/satici_admin.php'; }, );</script>";
+
+} 
+else if ($hesap_tipi == "admin") {
+    echo "<script>setTimeout(function() { window.location.href = '../users/admin.php'; }, );</script>";
+}
+else{
+    echo "<script>alert('böyle bir müşteri bulunmamakta');</script>";
+}
+
+exit();
+}
+
+
+
+
+else {
     // Giriş başarısız ise, kullanıcıyı tekrar giriş sayfasına yönlendir
     echo "<script>alert('Hatalı Giriş!');</script>";
     echo "<script>setTimeout(function() { window.location.href = '../login/login.php'; }, );</script>";
