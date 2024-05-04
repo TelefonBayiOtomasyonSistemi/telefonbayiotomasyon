fdac803a1a90b9e73a4762dbc8d940e7160b29e3
Doğukan Kıralı <kiralidogukan@gmail.com>
2024-05-04 17:45:05 +0300

    Ayarlar güncellendi
    admin,user,satici için bölümler oluşturuldu.

---
+++ Kodlar/php/users/bayi_yonetim/bayi_left_bar.php
@@ -0,0 +1,19 @@
+<h2>Ürün İşlemleri</h2>
+<ul>
+  <li><a href="../urun_islem/urun_gor.php">Ürün Görüntüleme</a></li>
+  <li><a href="../urun_islem/urun_degis.php">Ürün Düzenleme</a></li>
+  <li><a href="../urun_islem/urun_ekle.php">Ürün Ekleme</a></li>
+  <li><a href="../urun_islem/urun_sil.php">Ürün Silme</a></li>
+</ul>
+
+<h2>Bayi İşlemleri</h2>
+<ul>
+  <li><a href="../bayi_islem/bayi_gecmis_islem.php">Geçmiş İşlemler</a></li>
+</ul>
+
+<h2>Hesap İşlemleri</h2>
+<ul>
+  <li><a href="../hesap_islem/hesap_ayarlari_degis.php">Hesap Değiştir</a></li>
+  <li><a href="../hesap_islem/hesap_destek.php">Destek</a></li>
+  <li><a href="../hesap_islem/hesap_cikis.php">Çıkış Yap</a></li>
+</ul>
